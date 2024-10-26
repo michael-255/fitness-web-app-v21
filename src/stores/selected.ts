a880c3b2280f6c096b2d03c33009779450ad9175
Michael Joy <kodiakchaos@gmail.com>
2024-10-26 19:09:07 -0400

    updated selected store

--- src/stores/selected.ts
+++ src/stores/selected.ts
@@ -1,33 +1,27 @@
-import type { ExerciseType } from '@/models/Exercise'
-import type { ExerciseResultType } from '@/models/ExerciseResult'
-import type { LogType } from '@/models/Log'
-import type { MeasurementType } from '@/models/Measurements'
-import type { WorkoutType } from '@/models/Workout'
-import type { WorkoutResultType } from '@/models/WorkoutResult'
+import { StatusEnum } from '@/shared/enums'
 import { defineStore } from 'pinia'
 
 /**
- * Storing the currently selected records for any form and whether it is valid.
+ * Storing the currently selected record.
  */
 const useSelectedStore = defineStore({
     id: 'selected',
 
     state: () => ({
-        isLogValid: true,
-        isMeasurementValid: true,
-        isMeasurementResultValid: true,
-        isWorkoutValid: true,
-        isWorkoutResultValid: true,
-        isExerciseValid: true,
-        isExerciseResultValid: true,
-        log: {} as LogType, // Only needs this for inspection
-        measurement: {} as MeasurementType,
-        workout: {} as WorkoutType,
-        workoutResult: {} as WorkoutResultType,
-        exercise: {} as ExerciseType,
-        exerciseResult: {} as ExerciseResultType,
         record: {} as Record<string, any>,
     }),
+
+    getters: {
+        lockedStatus: (state): boolean => {
+            return state?.record?.status?.includes(StatusEnum.LOCKED) ?? false
+        },
+        favoritedStatus: (state): boolean => {
+            return state?.record?.status?.includes(StatusEnum.FAVORITED) ?? false
+        },
+        hiddenStatus: (state): boolean => {
+            return state?.record?.status?.includes(StatusEnum.HIDDEN) ?? false
+        },
+    },
 })
 
 export default useSelectedStore
