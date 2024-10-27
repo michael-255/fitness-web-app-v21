12a355382c94d83749e1685af1e279668de85c2f
Michael Joy <kodiakchaos@gmail.com>
2024-10-27 15:41:37 -0400

    adding plan keys enum

--- src/models/Plan.ts
+++ src/models/Plan.ts
@@ -1,19 +1,49 @@
-import { TableEnum } from '@/shared/enums'
-import { idSchema, timestampSchema } from '@/shared/schemas'
-import type { IdType, TimestampType } from '@/shared/types'
-import { createId } from '@/shared/utils'
+import { timestampSchema } from '@/shared/schemas'
+import type { TimestampType } from '@/shared/types'
 import { z } from 'zod'
 
 //
 // Enums
 //
 
+/**
+ * The only valid keys for plans in the application.
+ */
+export enum PlanKeyEnum {
+    // Specific weekday recurring
+    MONDAY = 'Monday',
+    TUESDAY = 'Tuesday',
+    WEDNESDAY = 'Wednesday',
+    THURSDAY = 'Thursday',
+    FRIDAY = 'Friday',
+    SATURDAY = 'Saturday',
+    SUNDAY = 'Sunday',
+    // Specific month recurring
+    JANUARY = 'January',
+    FEBRUARY = 'February',
+    MARCH = 'March',
+    APRIL = 'April',
+    MAY = 'May',
+    JUNE = 'June',
+    JULY = 'July',
+    AUGUST = 'August',
+    SEPTEMBER = 'September',
+    OCTOBER = 'October',
+    NOVEMBER = 'November',
+    DECEMBER = 'December',
+    // Long term recurring
+    MONTHLY = 'Monthly',
+    YEARLY = 'Yearly',
+}
+
 //
 // Schemas
 //
 
+export const planKeySchema = z.nativeEnum(PlanKeyEnum)
+
 export const planSchema = z.object({
-    id: idSchema,
+    key: planKeySchema,
     createdAt: timestampSchema,
     // TODO
 })
@@ -22,11 +52,14 @@ export const planSchema = z.object({
 // Types
 //
 
+export type PlanKeyType = z.infer<typeof planKeySchema>
+
 export type PlanType = z.infer<typeof planSchema>
 
 interface PlanParams {
-    id?: IdType
+    key: PlanKeyType
     createdAt?: TimestampType
+    // TODO
 }
 
 /**
@@ -35,11 +68,11 @@ interface PlanParams {
  *  Represents all relevant details of a plan.
  */
 export default class Plan {
-    id: IdType
+    key: PlanKeyType
     createdAt: TimestampType
 
     constructor(params: PlanParams) {
-        this.id = params.id ?? createId(TableEnum.PLANS)
+        this.key = params.key
         this.createdAt = params.createdAt ?? Date.now()
         // TODO
     }
